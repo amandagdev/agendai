5971ddb02482539da6411caca19687a7f7b40083
Amanda <amandago060@gmail.com>
2025-07-12 00:15:02 -0300

    feat: add tests setup

---
+++ src/app/page.spec.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react'
+import Page from './page'
+
+describe('Page', () => {
+  it('renders a heading', () => {
+    render(<Page />)
+
+    const heading = screen.getByText('Agendai')
+
+    expect(heading).toBeInTheDocument()
+  })
+})
